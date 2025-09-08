b7baacfa99f26dd4d117d557379c6572ff111692
shadyblues2006-d <ditichhaproo@gmail.com>
2025-09-08 12:54:19 -0500

    Setup for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/personal-website/' : '/',
-}))
+  base: '/', // <--- Important for a user site!
+})
